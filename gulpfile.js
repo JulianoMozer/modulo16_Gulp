280ac1dc57fec1b29f248c4bb4b4f9df332563a0
juliano <mozerlogistica@gmail.com>
2024-09-19 17:26:36 -0300

    Adicionar projeto completo com o arquivo Gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate');
+const imagemin = require('gulp-imagemin');
+
+// Função para comprimir imagens
+function comprimeImages() {
+    console.log("Iniciando a compressão de imagens...");
+    return gulp.src('./source/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/images'));
+}
+
+// Função para comprimir JavaScript
+function comprimeJavaScript() {
+    return gulp.src('./source/scripts/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/scripts'));
+}
+
+// Função para compilar Sass
+function compilaSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+// Exporta as tarefas
+exports.images = comprimeImages;
+exports.javascript = comprimeJavaScript;
+exports.sass = compilaSass;
+
+exports.default = function() {
+    gulp.watch('./source/styles/*.scss', { ignoreInitial: false }, gulp.series(compilaSass));
+    gulp.watch('./source/scripts/*.js', { ignoreInitial: false }, gulp.series(comprimeJavaScript));
+    gulp.watch('./source/images/*', { ignoreInitial: false }, gulp.series(comprimeImages));
+}
